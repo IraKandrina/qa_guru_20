0c03464dd94044ff3569ad09c7f47f79e1287d92
ikandrina <irina-kandrina@list.ru>
2023-07-15 18:43:27 +0300

    change java version

--- src/test/java/org/openstreetmap/OpenStreetMapTests.java
+++ src/test/java/org/openstreetmap/OpenStreetMapTests.java
@@ -1,6 +1,5 @@
 package org.openstreetmap;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
