4af312c9e4734c0de05849a402252cdb6c54f53d
ikandrina <irina-kandrina@list.ru>
2023-07-15 18:41:44 +0300

    enable osm tests

--- src/test/java/org/openstreetmap/OpenStreetMapTests.java
+++ src/test/java/org/openstreetmap/OpenStreetMapTests.java
@@ -15,7 +15,6 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
-@Disabled
 public class OpenStreetMapTests extends TestBase{
 
     @ValueSource (strings = {"GPS-треки", "Сообщества"})
