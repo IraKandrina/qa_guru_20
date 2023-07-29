7c018f208b307beb3e903a5d42ed02f5756bc91d
ikandrina <irina-kandrina@list.ru>
2023-07-29 17:18:55 +0300

    add tests for reqres.in

---
+++ src/test/java/in/reqres/ReqresTests.java
@@ -0,0 +1,87 @@
+package in.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class ReqresTests extends TestBase {
+    @Test
+    void createUserTest(){
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void getUserTest(){
+        given()
+                .log().all()
+                .when()
+                .get("/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void fullUpdateUserTest(){
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void partialUpdateUserTest(){
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion president\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .patch("/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion president"));
+    }
+
+    @Test
+    void deleteUserTest(){
+        given()
+                .log().all()
+                .when()
+                .delete("/users/2")
+                .then()
+                .log().all()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+
+}
+
---
+++ src/test/java/in/reqres/TestBase.java
@@ -0,0 +1,15 @@
+package in.reqres;
+
+import org.junit.jupiter.api.BeforeAll;
+import io.restassured.RestAssured;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+
+}
+
